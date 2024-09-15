2826744bd4750e09fd7c52691c08296e5063c0a6
jgray9 <jgray9@umbc.edu>
2024-09-14 20:08:36 -0400

    created heuristic function

--- gray_hw1.py
+++ gray_hw1.py
@@ -20,7 +20,21 @@ class StateSpace():
                 s += f'{self[x][y]:^5}'
             s += '\n'
         return s[:-1]
-        
+
+def h(x, y, goal_x, goal_y):
+    # move up
+    if y > goal_y:
+        return h(x, y - 1, goal_x, goal_y) + 1
+    # move right
+    if x < goal_x:
+        return h(x + 1, y, goal_x, goal_y) + 1
+    # move down
+    if y < goal_y:
+        return h(x, y + 1, goal_x, goal_y) + 1
+    # move left
+    if x > goal_x:
+        return h(x - 1, y, goal_x, goal_y) + 1
+    return 0
 
 def solve(initial_state, goal_state, problem):
     state_space = StateSpace(problem)
@@ -28,5 +42,12 @@ def solve(initial_state, goal_state, problem):
     print(f'Agent: {initial_state}')
     print(f'Goal State: {goal_state}')
     print(f'State Space:\n{state_space}')
+
+    print('Heuristics:')
+    for x in range(state_space.n):
+        for y in range(state_space.n):
+            dist = h(x, y, goal_state[0], goal_state[1])
+            print(f'{dist:^5}', end='')
+        print('')
 
 solve((1,0), (1,2), [[p,p,p], [p,m,p], [s,s,s]])
